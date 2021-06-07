663508bd1801283de065a689e880e007392e62ef
sunhao <sunhao@indemind.cn>
2021-06-07 17:06:47 +0800

    for capture tool

--- main.py
+++ main.py
@@ -10,37 +10,73 @@
 import sys, os
 import cv2
 import numpy as np
+from tqdm import tqdm
 
 CURRENT_DIR = os.path.dirname(__file__)
 sys.path.append(os.path.join(CURRENT_DIR, '../../'))
 
 import argparse
-from utils.imageprocess import Remap
-from utils.file import MkdirSimple
+from utils.imageprocess import Remap, ReadPara
+from utils.file import MkdirSimple, Walk
 
+CONFIG_FILE = 'config.yaml'
 
 def GetArgs():
     parser = argparse.ArgumentParser(description="",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--input", type=str, help="image file")
+    parser.add_argument("--input", type=str, help="image file or dir")
     parser.add_argument("--output_dir", type=str, default=None, help="output dir")
+    parser.add_argument("--flip", type=bool, default=False, help="flip up-down")
 
     args = parser.parse_args()
     return args
 
 
-def main():
-    args = GetArgs()
-    image = cv2.imread(args.input)
-    imageRemap = Remap(image)
-    if args.output_dir is not None:
-        output_file = os.path.join(args.output_dir, os.path.basename(args.input))
-        MkdirSimple(output_file)
-        cv2.imwrite(output_file, imageRemap)
-
+def RemapFile(file, flip, fisheye_x, fisheye_y):
+    image = cv2.imread(file)
+    if image is None:
+        print(file)
+        return
+    imageRemap = Remap(image, fisheye_x, fisheye_y)
     cv2.namedWindow("remap")
     cv2.imshow("remap", np.hstack((image, imageRemap)))
     cv2.waitKey(0)
+    if flip:
+        imageRemap = imageRemap[::-1, :, :]
+
+    return imageRemap
+
+def WriteImage(image, file, output_dir, root_len):
+    if output_dir is not None:
+        sub_path = file[root_len+1:]
+        output_file = os.path.join(output_dir, sub_path)
+        MkdirSimple(output_file)
+        cv2.imwrite(output_file, image)
+
+def main():
+    args = GetArgs()
+
+    if os.path.isfile(args.input):
+        root = len(os.path.dirname(args.input))
+        fisheye_x, fisheye_y = ReadPara(os.path.join(os.path.dirname(args.input), CONFIG_FILE))
+        imageRemap = RemapFile(args.input, args.flip, fisheye_x, fisheye_y)
+        WriteImage(imageRemap, args.input, args.output_dir , root)
+    else:
+        root = len(os.path.dirname(args.input))
+        dirs = os.listdir(args.input)
+        for d in dirs:
+            print("in dir: ", d)
+            files = Walk(os.path.join(args.input, d), ['jpg', 'png'])
+            config_file = os.path.join(os.path.join(args.input, d), CONFIG_FILE)
+            fisheye_x, fisheye_y = ReadPara(config_file)
+
+            for f in tqdm(files):
+                imageRemap = RemapFile(f, args.flip, fisheye_x, fisheye_y)
+                if imageRemap is None:
+                    print(f)
+                    continue
+                WriteImage(imageRemap, f, args.output_dir , root)
+
 
 if __name__ == '__main__':
     main()
--- utils/imageprocess.py
+++ utils/imageprocess.py
@@ -9,26 +9,23 @@
 '''
 import cv2
 import numpy as np
-
-K = [(7.1261170761727135e+02)/2, 0., (6.4968264752036998e+02)/2
-    , 0.,(7.1253440977996888e+02)/2, (4.1337075038430754e+02)/2, 0., 0., 1.]
-R = [(9.9985787572528106e-01), (5.3832163198578422e-03)
-    , (-1.5976524408707376e-02), (-5.4883259985329599e-03)
-    , (9.9996353651168690e-01), (-6.5424708462575197e-03)
-    , (1.5940722313064530e-02), (6.6292253766121224e-03)
-    , (9.9985096226539882e-01)]
-D = [(-5.6028484984777541e-02), (-9.3122797502125758e-03)
-    , (-1.4479074254791148e-02), (1.0911557039419725e-02)]
-P = [620.138/2, 0., 698.277/2, 0, 0, 620.1377/2
-    , 433.271/2, 0., 0., 0., 1., 0]
+import os
 
 W, H  = 640, 400
 
-def ReadPara():
-    k = np.reshape(K, (3, 3))
-    d = np.reshape(D, (4, 1))
-    r = np.reshape(R, (3, 3))
-    p = np.reshape(P, (3, 4))
+def ReadPara(file):
+    if not os.path.exists(file):
+        print("not exist file <", file, ">.")
+        exit(0)
+
+    config = cv2.FileStorage(file, cv2.FILE_STORAGE_READ)
+
+    r = config.getNode('Rl').mat()
+    p = config.getNode('Pl').mat()
+    k = config.getNode('Kl').mat()
+    d = config.getNode('Dl').mat()
+
+    config.release()
 
     fisheye_x, fisheye_y = np.ndarray((H, W), np.float32), np.ndarray((H, W), np.float32)
 
@@ -36,9 +33,8 @@ def ReadPara():
 
     return fisheye_x, fisheye_y
 
-fisheye_x, fisheye_y = ReadPara()
 
-def Remap(image):
+def Remap(image, fisheye_x, fisheye_y):
     imgaeRemap =  cv2.remap(image, fisheye_x, fisheye_y, cv2.INTER_LINEAR)
     return imgaeRemap
 
